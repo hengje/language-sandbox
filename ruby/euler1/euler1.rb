b3f6acdc6054ee0265d0faff3340337d65c10211
Henning Gjetanger <henning.gjetanger@gmail.com>
2012-09-18 19:21:53 +0200

    Use select and reduce in Ruby implementation of euler1 problem

--- ruby/euler1/euler1.rb
+++ ruby/euler1/euler1.rb
@@ -1,7 +1,3 @@
-sum = 0
-(1..999).each do |i|
-    if i % 3 == 0 || i % 5 == 0
-        sum += i
-    end
-end
+sum = (1..999).select{|x|x%3==0||x%5==0}.reduce(:+)
 puts "Sum of all integers divisible by 3 or 5 below 1000: " + sum.to_s
+
